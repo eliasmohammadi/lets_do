1fcb5593158439c8e56da16caa592fe4df2ebc7c
Elias Mohammadi <elias.mohammady91@gmail.com>
2023-03-01 19:18:37 +0330

    add mapper in generic form to convet domain to dto

---
+++ src/utils/mapper.ts
@@ -0,0 +1,8 @@
+export class Mapper {
+    from<T, U>(t:T,cb:Mapper.MapFunction<T, U>): U {
+        return cb(t)
+    }
+}
+namespace Mapper {
+    export type MapFunction<T, U> = (t:T) => U
+}
---
+++ test/utils/mapper.test.ts
@@ -0,0 +1,32 @@
+import { Mapper } from "../../src/utils/mapper"
+
+describe("Mapper", () => {
+    it("should map and convert type T to U", () => {
+
+        type FirstType = {
+            first_name: string
+            last_name: string
+        }
+        type SecondType = {
+            fullName: string
+        }
+
+        const obj: FirstType = {
+            first_name:"Elias",
+            last_name: "Mohammadi"
+        }
+
+        const actual = new Mapper().from<FirstType, SecondType>(obj,(o) => {
+            return {
+                fullName: `${o.first_name} ${o.last_name}`
+            }
+        })
+
+        expect(actual.fullName).toEqual(`${obj.first_name} ${obj.last_name}`)
+
+
+
+
+
+    })
+})
